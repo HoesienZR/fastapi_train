de2474a3d247e1446cc69f9b58a9c5d831e39444
HosseinZFR <ghost.zr4848@gmail.com>
2024-10-03 11:57:27 +0330

    created category schema for post

--- schema/post/input.py
+++ schema/post/input.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
-from uuid import UUID
 
+from uuid import UUID
+from enum import Enum
+class PostCategory(str,Enum):
+    Science  = 'Science'
+    Computer = 'Computer'
+    Mechanic = 'Mechanic'
+    Others   =  'Others'
 class BasePostInput(BaseModel):
     """schema of post when it will be retrieved """
     id:UUID
@@ -8,3 +14,7 @@ class PostInput(BasePostInput):
     """schema of post when it created """
     title:str
     description:str
+class CreatePostInput(BaseModel):
+    title:str
+    description:str
+    category:PostCategory
--- schema/post/output.py
+++ schema/post/output.py
@@ -2,10 +2,13 @@ from pydantic import BaseModel
 from sqlalchemy import Date
 from datetime import date
 
+from enum import Enum
 from uuid  import UUID
+from .input import PostCategory
 
 class PostOutput(BaseModel):
     """output schema of post that our api routers return """
     title:str
     description:str
     id:UUID
+    category:PostCategory
