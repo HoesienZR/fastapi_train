1a3462657d0d7edadafb2678f4cfbcfab9e14faf
HosseinZFR <ghost.zr4848@gmail.com>
2024-10-03 11:51:53 +0330

    added category to get_post

--- opreation/Blog.py
+++ opreation/Blog.py
@@ -8,10 +8,11 @@ class BlogOperation:
      def __init__(self, db_session: AsyncSession) -> None:
         self.db_session = db_session
 
-     async def create_post(self,post:post_input.PostInput)->post_output.PostOutput:
+     async def create_post(self,post:post_input.CreatePostInput)->post_output.PostOutput:
         post = Post(#user_id=uuid4(),
                     title=post.title,
                     description=post.description,
+                    category = post.category
         )
         await PostQueries.create_post(post=post,db_session=self.db_session)
         return post_output.PostOutput(
@@ -19,6 +20,7 @@ class BlogOperation:
                           title=post.title,
                           description=post.description,
                           id = post.id,
+                          category= post.category
         )
      async def get_all_posts(self):
          posts = await PostQueries.get_all_posts(db_session=self.db_session)
@@ -27,14 +29,17 @@ class BlogOperation:
          post_data = await PostQueries.get_post(db_session=self.db_session,post_id=post_id)
          return post_output.PostOutput(title = post_data.title,
                                        description = post_data.description,
-                                       id = post_data.id)
+                                       id = post_data.id,
+                                       category = post_data.category
+                                       )
      async def delete_post(self,post_id:UUID)->None:
          await PostQueries.delete_post(db_session=self.db_session,post_id=post_id)
      async def update_post(self,post:post_input.PostInput)->post_output.PostOutput:
         post = await PostQueries.update_post(db_session=self.db_session,post=post)
         return post_output.PostOutput(title = post.title,
                                       id = post.id,
-                                      description=post.description)
+                                      description=post.description,
+                                      category = Post.category)
 
 
 
